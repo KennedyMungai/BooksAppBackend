2898a5c91300e68be4426dbff0a19d178a508b76
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-29 14:41:50 +0300

    Created the database module

---
+++ database/db.py
@@ -0,0 +1,14 @@
+"""The database config file"""
+from beanie import init_beanie
+import motor.motor_asyncio
+from models.models import Book
+
+
+async def init_db():
+    """The function that initializes the database"""
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        "mongodb://localhost:27017")
+    await init_beanie(
+        database=client.books_collection,
+        document_models=[Book],
+    )
