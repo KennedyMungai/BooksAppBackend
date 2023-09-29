c1cc80c02d65e6d22646a403879a4aa5f75c99d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-29 14:57:30 +0300

    Wrote the endpoint to retrieve all the books in the database

--- routers/books.py
+++ routers/books.py
@@ -1,4 +1,5 @@
 """The Books Route Logic"""
+from typing import List
 from fastapi import APIRouter, FastAPI, HTTPException, status
 
 from models.models import Book
@@ -15,3 +16,18 @@ async def create_book(book: Book):
     """Create a new book"""
     await book.insert()
     return book
+
+
+@books_router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[Book]
+)
+async def retrieve_all_books() -> List[Book]:
+    """The endpoint to retrieve all the books in the database
+
+    Returns:
+        List[Book]: The list of all the books in the database
+    """
+    books = await Book.find_all().to_list()
+    return books
